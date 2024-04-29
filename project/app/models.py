2bc83a833e164f7bb023e6287c21363b1e2f3b72
Juan Esteban A <juanesaristy16@hotmail.com>
2024-04-28 20:24:39 -0500

    Cambio paiss a paises

--- project/app/models.py
+++ project/app/models.py
@@ -12,7 +12,10 @@ class Cliente(models.Model):
     nombre = models. CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     nacimiento = models.DateField(null=True)
-    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL,blank=True, null=True)
+    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL,blank=True, null=True, verbose_name="Pais de origen")
 
     def __str__(self):
         return f"{self.nombre}, {self.apellido}"
+    
+    class meta:
+        verbose_name_plural = "Paises"
