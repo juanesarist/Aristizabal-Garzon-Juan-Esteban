2e2301523aaef79e75502547b3ad09b41879e65f
Juan Esteban A <juanesaristy16@hotmail.com>
2024-05-19 18:29:50 -0500

    Cambios buscar en class

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,14 +5,15 @@ from . import views
 app_name = "app"
 
 urlpatterns = [
-    path('', views.buscar_pacientes, name="home"),
-    path('pacientes', views.pacientes, name="registro_pacientes"),
+    path('', views.mostrar_pacientes, name="home"),
+    path('pacientes', views.pacientes_create.as_view(), name="registro_pacientes"),
     path("medicos", views.personal_medico, name="registro_medicos"),
     path("personaladm", views.personal_adm, name="registro_personaladm"),
     path('mostarpacientes', views.mostrar_pacientes, name="mostrar_pacientes"),
-    path('mostarpacientes/detail/<int:pk>', views.buscar_pacientes_info, name="mostrar_pacientes_detail"),
-    path('mostarpacientes/update/<int:pk>', views.buscar_pacientes_update, name="mostrar_pacientes_update"),
-    path('mostarpacientes/delete/<int:pk>', views.buscar_pacientes_delete, name="mostrar_pacientes_delete"),
+    path('buscarpacientes', views.buscar_pacientes.as_view(), name="buscar_pacientes"),
+    path('mostarpacientes/detail/<int:pk>', views.buscar_pacientes_info.as_view(), name="mostrar_pacientes_detail"),
+    path('mostarpacientes/update/<int:pk>', views.buscar_pacientes_update.as_view(), name="mostrar_pacientes_update"),
+    path('mostarpacientes/delete/<int:pk>', views.buscar_pacientes_delete.as_view(), name="mostrar_pacientes_delete"),
     path("mostarmedicos", views.mostrar_personal_medico, name="mostrar_medicos"),
     path("mostrarpersonaladm", views.mostrar_personal_adm, name="mostrar_personaladm"),
     
--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from . import models, forms
+
 # Create your views here.
 
 def mostrar_pacientes(request):
@@ -17,16 +20,7 @@ def mostrar_personal_adm(request):
     context = {"personaladm": consulta_personal_adm}
     return render(request, "app/mostrarpersonaladm.html", context)
 
-def pacientes(request):
-    if request.method == "POST":
-        form = forms.pacienteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("app:registro_pacientes")
-    else:
-        form = forms.pacienteForm()
 
-    return render(request, "app/registropacientes.html", context = {"form":form})
 
 def personal_medico(request):
     if request.method == "POST":
@@ -50,35 +44,75 @@ def personal_adm(request):
 
     return render(request, "app/registropersonaladm.html", context = {"form":form})
 
-def buscar_pacientes(request):
-     consulta = request.GET.get("consulta", None)
-     if consulta:
-         print(consulta)
-         query = models.Pacientes.objects.filter(cedula__icontains=consulta)
-     else:
-         query = models.Pacientes.objects.all()
-     context = {"pacientes": query}
-     return render(request, "app/mostrarpacientes.html", context)
+# def buscar_pacientes(request):
+#      consulta = request.GET.get("consulta", None)
+#      if consulta:
+#          print(consulta)
+#          query = models.Pacientes.objects.filter(cedula__icontains=consulta)
+#      else:
+#          query = models.Pacientes.objects.all()
+#      context = {"pacientes": query}
+#      return render(request, "app/mostrarpacientes.html", context)
 
-def buscar_pacientes_info(request, pk):
-     query = models.Pacientes.objects.get(id=pk)
-     return render(request, "app/mostrarpacientes_detail.html", {"paciente":query})
+class buscar_pacientes(ListView):
+     model = models.Pacientes
+     context_object_name = "pacientes"
+     template_name = "app/mostrarpacientes.html"
 
-def buscar_pacientes_update(request, pk = int):
-    query = models.Pacientes.objects.get(id=pk) 
-    if request.method == "POST":
-        form = forms.pacienteForm(request.POST, instance=query)
-        if form.is_valid():
-            form.save()
-            return redirect("app:home")
-    else:
-        form = forms.pacienteForm(instance=query)
 
-    return render(request, "app/mostrarpacientes_update.html", context = {"form":form})
+# def buscar_pacientes_info(request, pk):
+#      query = models.Pacientes.objects.get(id=pk)
+#      return render(request, "app/mostrarpacientes_detail.html", {"paciente":query})
 
-def buscar_pacientes_delete(request, pk = int):
-    query = models.Pacientes.objects.get(id=pk) 
-    if request.method == "POST":
-        query.delete()
-        return redirect("app:home")
-    return render(request, "app/mostrarpacientes_delete.html", context = {"paciente":query})
+class buscar_pacientes_info(DetailView):
+     model = models.Pacientes
+     context_object_name = "paciente"
+     template_name = "app/mostrarpacientes_detail.html"
+
+class pacientes_create(CreateView):
+     model = models.Pacientes
+     form_class = forms.pacienteForm
+     template_name = "app/registropacientes.html"
+     success_url = reverse_lazy("app:registro_pacientes")
+     
+
+# def pacientes(request):
+#     if request.method == "POST":
+#         form = forms.pacienteForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("app:registro_pacientes")
+#     else:
+#         form = forms.pacienteForm()
+
+#     return render(request, "app/registropacientes.html", context = {"form":form})
+
+class buscar_pacientes_update(UpdateView):
+     model = models.Pacientes
+     form_class = forms.pacienteForm
+     template_name = "app/registropacientes.html"
+     success_url = reverse_lazy("app:buscar_pacientes")
+
+# def buscar_pacientes_update(request, pk = int):
+#     query = models.Pacientes.objects.get(id=pk) 
+#     if request.method == "POST":
+#         form = forms.pacienteForm(request.POST, instance=query)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("app:home")
+#     else:
+#         form = forms.pacienteForm(instance=query)
+
+#     return render(request, "app/mostrarpacientes_update.html", context = {"form":form})
+
+# def buscar_pacientes_delete(request, pk = int):
+#     query = models.Pacientes.objects.get(id=pk) 
+#     if request.method == "POST":
+#         query.delete()
+#         return redirect("app:home")
+#     return render(request, "app/mostrarpacientes_delete.html", context = {"paciente":query})
+
+class buscar_pacientes_delete(DeleteView):
+    model = models.Pacientes
+    template_name = "app/mostrarpacientes_delete.html"
+    success_url = reverse_lazy("app:buscar_pacientes")
