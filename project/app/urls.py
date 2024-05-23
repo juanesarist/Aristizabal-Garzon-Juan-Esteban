a60579778d6fd6bd30166b9b1232a8ee814f042a
Juan Esteban A <juanesaristy16@hotmail.com>
2024-05-22 22:46:41 -0500

    Actualizacion funcionamiento pag

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,17 +5,17 @@ from . import views
 app_name = "app"
 
 urlpatterns = [
-    path('', views.mostrar_pacientes, name="home"),
+    #path('', views.mostrar_pacientes, name="home"),
     path('pacientes', views.pacientes_create.as_view(), name="registro_pacientes"),
-    path("medicos", views.personal_medico, name="registro_medicos"),
-    path("personaladm", views.personal_adm, name="registro_personaladm"),
-    path('mostarpacientes', views.mostrar_pacientes, name="mostrar_pacientes"),
+    path("aboutus/", views.about_us, name="about_us"),
+    #path("personaladm", views.personal_adm, name="registro_personaladm"),
+    #path('mostarpacientes', views.mostrar_pacientes, name="mostrar_pacientes"),
     path('buscarpacientes', views.buscar_pacientes.as_view(), name="buscar_pacientes"),
     path('mostarpacientes/detail/<int:pk>', views.buscar_pacientes_info.as_view(), name="mostrar_pacientes_detail"),
     path('mostarpacientes/update/<int:pk>', views.buscar_pacientes_update.as_view(), name="mostrar_pacientes_update"),
     path('mostarpacientes/delete/<int:pk>', views.buscar_pacientes_delete.as_view(), name="mostrar_pacientes_delete"),
-    path("mostarmedicos", views.mostrar_personal_medico, name="mostrar_medicos"),
-    path("mostrarpersonaladm", views.mostrar_personal_adm, name="mostrar_personaladm"),
+    #path("mostarmedicos", views.mostrar_personal_medico, name="mostrar_medicos"),
+    #path("mostrarpersonaladm", views.mostrar_personal_adm, name="mostrar_personaladm"),
     
 
     
--- project/app/views.py
+++ project/app/views.py
@@ -7,54 +7,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-def mostrar_pacientes(request):
-    consulta_pacientes = models.Pacientes.objects.all()
-    context = {"pacientes": consulta_pacientes}
-    return render(request, "app/mostrarpacientes.html", context)
-
-def mostrar_personal_medico(request):
-    consulta_medicos = models.Medicos.objects.all()
-    context = {"medicos": consulta_medicos}
-    return render(request, "app/mostrarmedicos.html", context)
-
-def mostrar_personal_adm(request):
-    consulta_personal_adm = models.Pacientes.objects.all()
-    context = {"personaladm": consulta_personal_adm}
-    return render(request, "app/mostrarpersonaladm.html", context)
-
-
-
-def personal_medico(request):
-    if request.method == "POST":
-            form = forms.medicosForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect("app:registro_medicos")
-    else:
-            form = forms.medicosForm()
-
-    return render(request, "app/registromedico.html", context = {"form":form})
-
-def personal_adm(request):
-    if request.method == "POST":
-            form = forms.personaladmForm(request.POST)
-            if form.is_valid():
-                form.save()
-                return redirect("app:registro_personaladm")
-    else:
-            form = forms.personaladmForm()
-
-    return render(request, "app/registropersonaladm.html", context = {"form":form})
-
-# def buscar_pacientes(request):
-#      consulta = request.GET.get("consulta", None)
-#      if consulta:
-#          print(consulta)
-#          query = models.Pacientes.objects.filter(cedula__icontains=consulta)
-#      else:
-#          query = models.Pacientes.objects.all()
-#      context = {"pacientes": query}
-#      return render(request, "app/mostrarpacientes.html", context)
+def about_us(request):
+    return render(request, "app/aboutus.html")
 
 class buscar_pacientes(LoginRequiredMixin, ListView):
      model = models.Pacientes
@@ -70,9 +24,7 @@ class buscar_pacientes(LoginRequiredMixin, ListView):
         return pacientes
 
 
-# def buscar_pacientes_info(request, pk):
-#      query = models.Pacientes.objects.get(id=pk)
-#      return render(request, "app/mostrarpacientes_detail.html", {"paciente":query})
+
 
 class buscar_pacientes_info(LoginRequiredMixin, DetailView):
      model = models.Pacientes
@@ -86,16 +38,7 @@ class pacientes_create(LoginRequiredMixin, CreateView):
      success_url = reverse_lazy("app:registro_pacientes")
      
 
-# def pacientes(request):
-#     if request.method == "POST":
-#         form = forms.pacienteForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("app:registro_pacientes")
-#     else:
-#         form = forms.pacienteForm()
 
-#     return render(request, "app/registropacientes.html", context = {"form":form})
 
 class buscar_pacientes_update(LoginRequiredMixin, UpdateView):
      model = models.Pacientes
@@ -103,24 +46,7 @@ class buscar_pacientes_update(LoginRequiredMixin, UpdateView):
      template_name = "app/registropacientes.html"
      success_url = reverse_lazy("app:buscar_pacientes")
 
-# def buscar_pacientes_update(request, pk = int):
-#     query = models.Pacientes.objects.get(id=pk) 
-#     if request.method == "POST":
-#         form = forms.pacienteForm(request.POST, instance=query)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("app:home")
-#     else:
-#         form = forms.pacienteForm(instance=query)
 
-#     return render(request, "app/mostrarpacientes_update.html", context = {"form":form})
-
-# def buscar_pacientes_delete(request, pk = int):
-#     query = models.Pacientes.objects.get(id=pk) 
-#     if request.method == "POST":
-#         query.delete()
-#         return redirect("app:home")
-#     return render(request, "app/mostrarpacientes_delete.html", context = {"paciente":query})
 
 class buscar_pacientes_delete(LoginRequiredMixin, DeleteView):
     model = models.Pacientes
